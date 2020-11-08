01e0a766e498970c1ced308e2b384735450e1e38
4ked <max.goeke0@gmail.com>
2020-11-08 12:45:18 -0500

    fixing index path

--- root/backend/server.js
+++ root/backend/server.js
@@ -2,8 +2,6 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 
-const server = http.createServer(app);
-const port = 3000;
 
 const app = express();
 
@@ -12,9 +10,12 @@ app.use(express.static("express"));
 
 // default URL for website
 app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/root/frontend/index.html'));
+    res.sendFile(path.join(__dirname+'../../frontend/index.html'));
     //__dirname : It will resolve to your project folder.
-  });
+});
+
+const server = http.createServer(app);
+const port = 3000;
 
 server.listen(port);
 console.debug('Server listening on port ' + port);
